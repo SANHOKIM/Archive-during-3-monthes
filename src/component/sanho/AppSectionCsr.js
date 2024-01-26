55613e872ea7647d70b3f2f10834caa78523b375
SANHOKIM <awrko@naver.com>
2024-01-26 17:23:43 +0900

    enhance : 피셔-예이츠 셔플 알고리즘을 사용해서 중복 없이 배열을 무작위로 섞을 수 있게 개선

--- src/component/sanho/AppSectionCsr.js
+++ src/component/sanho/AppSectionCsr.js
@@ -3,11 +3,10 @@ import datajson from "../../data/data.json";
 import "./sanho.scss";
 
 function AppSectionCsr() {
-  const shuffledTaglist = datajson.sectioncsr.content.sort(
-    () => Math.random() - 0.5
-  );
+  const tagList = datajson.sectioncsr.content.slice(); // 원본 배열 복사
+  const shuffledTaglist = shuffleArray(tagList);
 
-  console.log(shuffledTaglist);
+  console.log("ㄱㅁㅅ", shuffledTaglist);
 
   return (
     <section id="csr">
@@ -15,42 +14,45 @@ function AppSectionCsr() {
         <h2 className="">{datajson.sectioncsr.h2}</h2>
         <div className="box1">
           <ul className="article-list d-lg-flex justify-content-between align-items-stretch">
-            {shuffledTaglist.map(
-              (el, idx) =>
-                idx < 3 && (
-                  <li className="show col-md-4 d-flex flex-column " key={idx}>
-                    <a href={el.href}>
-                      <div className="img-box">
-                        <img src={el.src} alt="펫푸드"></img>
-                      </div>
-                    </a>
+            {shuffledTaglist.slice(0, 3).map((el, idx) => (
+              <li className="show col-md-4 d-flex flex-column " key={idx}>
+                <a href={el.href}>
+                  <div className="img-box">
+                    <img src={el.src} alt="펫푸드"></img>
+                  </div>
+                </a>
 
-                    <div className="flex-grow-1 d-flex flex-column justify-content-between">
-                      <h3>
-                        <a href={el.href}>{el.h3}</a>
-                      </h3>
+                <div className="flex-grow-1 d-flex flex-column justify-content-between">
+                  <h3>
+                    <a href={el.href}>{el.h3}</a>
+                  </h3>
 
-                      <div className="csr-tag">
-                        <ul className="">
-                          {el.csrtag.map((eel, iidx) => {
-                            return (
-                              <li className="d-block mb-0">
-                                <a href={el.href} key={iidx}>
-                                  {eel.nm}
-                                </a>
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      </div>
-                    </div>
-                  </li>
-                )
-            )}
+                  <div className="csr-tag">
+                    <ul className="">
+                      {el.csrtag.map((eel, iidx) => (
+                        <li className="d-block mb-0" key={iidx}>
+                          <a href={el.href}>{eel.nm}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </section>
   );
 }
+
+// 피셔-예이츠 셔플 알고리즘을 사용하여 배열을 섞는 함수
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 export default AppSectionCsr;
